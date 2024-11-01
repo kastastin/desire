4fcdb5949f1db4f713b271574afef7ce7b09fe59
kastastin <kastastin.main@gmail.com>
2024-11-01 16:30:02 +0200

    add top section (1920px)

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,11 @@ function images() {
 }
 
 function scripts() {
-  return src(["node_modules/jquery/dist/jquery.js", "src/js/script.js"])
+  return src([
+    "node_modules/jquery/dist/jquery.js",
+    "node_modules/slick-carousel/slick/slick.js",
+    "src/js/script.js"
+  ])
     .pipe(concat("script.min.js"))
     .pipe(uglify())
     .pipe(dest("src/js"))
