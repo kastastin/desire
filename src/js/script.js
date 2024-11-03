e6da47e4ca8515c70eca575b8dd3a97fcacc2bc4
kastastin <kastastin.main@gmail.com>
2024-11-03 19:53:24 +0200

    add full blog page (1920px)

--- src/js/script.js
+++ src/js/script.js
@@ -21,6 +21,13 @@ $(function () {
     arrows: false,
   });
 
+  $(".article-slider__box").slick({
+    prevArrow:
+      '<button type="button" class="article-slider__arrow article-slider__arrow-left"><img src="images/arrow-slide-left.svg" alt="left arrow"></button>',
+    nextArrow:
+      '<button type="button" class="article-slider__arrow article-slider__arrow-right"><img src="images/arrow-slide-right.svg" alt="right arrow"></button>',
+  });
+
   if ($(".gallery__inner").length) {
     const mixer = mixitup(".gallery__inner", {
       load: {
