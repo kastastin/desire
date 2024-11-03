a060e6935e0ab5e1208645541c23789f557b9070
kastastin <kastastin.main@gmail.com>
2024-11-03 11:24:54 +0200

    add about section (1920px, fancyapps lib)

--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,7 @@ function scripts() {
     "node_modules/jquery/dist/jquery.js",
     "node_modules/slick-carousel/slick/slick.js",
     "node_modules/mixitup/dist/mixitup.js",
+    "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js",
     "src/js/script.js",
   ])
     .pipe(concat("script.min.js"))
