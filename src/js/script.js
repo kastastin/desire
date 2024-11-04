e1b8f9064775094848844f08335812c03c910e36
kastastin <kastastin.main@gmail.com>
2024-11-04 11:42:07 +0200

    add home page adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -13,6 +13,10 @@ $(function () {
     $(".menu").toggleClass("menu--open");
   });
 
+  if ($(window).width() <= 651) {
+    $(".works-path__item--measuring").appendTo($(".works-path__items-box"));
+  }
+
   $(".top__slider").slick({
     dots: true,
     arrows: false,
